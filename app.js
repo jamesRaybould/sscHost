c36057d9c2a030b76f8bedfa6d807020049e8c4d
James Raybould <James.Raybould@sky.uk>
2015-12-15 15:53:40 +0000

    Added basic templates & implementation of the client api

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express'),
     http = require('http'),
     path = require('path');
+var Client = require('components-client');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
 app.get('/', function(request, response) {
-  response.status(200).send(JSON.stringify({}));
+  var client = new Client(['sales-service']);
+  client.fetch().then(function() {
+    sscHTML = client.getHtml('sales-service');
+    sscCSS = client.getCss('sales-service');
+    sscJS = client.getJs('sales-service');
+    // console.log(sscHTML, sscJS, sscCSS)
+    response.status(200).render('index', {ssc: sscHTML, sscCSS: sscCSS, sscJS: sscJS});
+  });
+
 });
 
 var server = app.listen(3001, function () {
